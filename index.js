dd5fe7ee4fd6e810779e6b6155a28d37641f2761
Daniel Williams <danielpaulwill@gmail.com>
2021-12-10 15:51:55 -0500

    2nd event listener added. Starting coding on mouseover info

--- index.js
+++ index.js
@@ -12,30 +12,34 @@ function initFetch() {
   .then(data => cardCreator(data))
 }
 
-
-
 function cardCreator(array) {
   let cardArea = document.getElementById('cardArea')
   array.forEach(coffee => {
+    let coffeeInfo = document.createElement('li')
+      coffeeInfo.style = 'list-style: none;'
     let coffeeCard = document.createElement('li')
+    coffeeCard.addEventListener('mouseover', e => console.log(e))
+      coffeeCard.style = 'list-style: none;'
       coffeeCard.className = 'coffeeCard'
-    let coffeeTitle = document.createElement('h5')
-      coffeeTitle.innerText = coffee.name
-      coffeeCard.append(coffeeTitle)
+    // let coffeeTitle = document.createElement('h5')
+    //   coffeeTitle.innerText = coffee.name
+    //   coffeeCard.append(coffeeTitle)
     let coffeeImage = document.createElement('img')
       coffeeImage.className = 'coffeeCardImage'
       coffeeImage.src = coffee.image
       coffeeCard.append(coffeeImage)
-    let coffeeServing = document.createElement('p')
-      coffeeServing.innerText = `Serving Size: ${coffee.servingSize} fl oz`
-      coffeeCard.append(coffeeServing)
-    let coffeeCaffeine = document.createElement('p')
-      coffeeCaffeine.innerText = coffee.caffeineContent
-      coffeeCard.append(coffeeCaffeine)
-    let coffeeSugar = document.createElement('p')
-      coffeeSugar.innerText = coffee.sugarContent
-      coffeeCard.append(coffeeSugar)
+    // let coffeeServing = document.createElement('p')
+    //   coffeeServing.innerText = `Serving Size: ${coffee.servingSize} fl oz`
+    //   coffeeCard.append(coffeeServing)
+    // let coffeeCaffeine = document.createElement('p')
+    //   coffeeCaffeine.innerText = coffee.caffeineContent
+    //   coffeeCard.append(coffeeCaffeine)
+    // let coffeeSugar = document.createElement('p')
+    //   coffeeSugar.innerText = coffee.sugarContent
+    //   coffeeCard.append(coffeeSugar)
     console.log(coffeeCard)
+    let lineBreak = document.createElement('br')
+    cardArea.append(lineBreak)
     cardArea.append(coffeeCard)
     return coffeeCard
   })
